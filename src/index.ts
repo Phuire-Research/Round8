b04d8e84769fb9b9076f936d40958f341a074548
Micah Keller <micahkeller@zenwraith.local>
2025-11-11 11:50:10 -0800

    Working Checkpoing

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,3 @@
  * @version 0.0.11
  * @license GPL-3.0
  */
-
-// Export everything from Round8Binary model (v0.0.11 main API)
-export * from './concepts/round8/model/Round8Binary.model';
-
-// Export validated arithmetic operations and lookup tables
-export {
-  STRING_TO_ROUND8_ROTATION,
-  ROUND8_TO_STRING_ROTATION,
-  ROUND8_TO_STRING_SHIFTED_ROTATION,
-  SpooledRegularStringLookup,
-  SpooledShiftedStringLookup,
-  SumWrung,
-  DifferenceWrung
-} from './concepts/round8/model/Round8.cases';
-
-// Export BidirectionalConference (already re-exported by Round8Binary.model)
-// Available as: import { BidirectionalConference, MarqueeState } from 'round8';
